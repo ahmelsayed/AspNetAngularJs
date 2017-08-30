4e63c82f3139d23ac84ad8eab5f6308398444e32
Ahmed ElSayed <ahmels@microsoft.com>
2017-08-30 15:20:07 -0700

    Initial

---
+++ AspNetAngularJs/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AspNetAngularJs.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/<controller>
+        public async Task<IEnumerable<string>> Get()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AspNetAngularJs/ng/site.js
@@ -0,0 +1,16 @@
+﻿angular.module('aspNetAngularJsApp', [])
+    .controller('HelloController', function ($http) {
+        var _this = this;
+        _this.title = 'Hello from Angular';
+        _this.myList = ['Apple', 'Banana', 'Plum', 'Figs'];
+        _this.value = 'loading ...';
+
+        $http.get('api/values')
+            .then(function (response) {
+                console.log(response);
+                _this.value = response.data;
+            }, function (error) {
+                console.error(error);
+                _this.value = 'there was an error'
+            });
+    });
